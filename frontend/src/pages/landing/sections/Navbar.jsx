453e164534195ac2beeb0486cd572b61dca825b0
Naman <namansingla830@gmail.com>
2026-01-26 17:27:08 +0530

    navbar css completed

--- frontend/src/pages/landing/sections/Navbar.jsx
+++ frontend/src/pages/landing/sections/Navbar.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 
-import Logo from "../../../assets/logo/youtubeLogo.png"
+import Logo from "../../../assets/logo/Youtube_logo.png"
 
 const Navbar = () => {
   return (
     <nav className="navbar_container w-full py-5 fixed bg-black text-white" >
-        <div className="navbar flex justify-between items-center mx-10">
+        <div className="navbar flex justify-between items-center mx-16">
             <div className="navbar_logo flex items-center gap-2">
-                <figure className='w-20 '>
+                <figure className='w-12 '>
                     <img src={Logo} alt="navbar logo" className='img'/>
                 </figure>
-                <h2>CREATORSTUDIO</h2>
+                <h2 className='text-[22px] leading-6 font-bold'>CREATORSTUDIO</h2>
             </div>
 
-            <div className="navbar_cta">
-                <Link to="/">Log In</Link>
-                <Link to="/">Sign Up Free</Link>
+            <div className="navbar_cta flex items-center gap-8">
+                <Link to="/" className='text-[16px] font-medium leading-5'>Log In</Link>
+                <Link to="/" className='btn_primary'>Sign Up Free</Link>
             </div>
         </div>
     </nav>
