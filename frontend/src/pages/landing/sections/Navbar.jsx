b191302f7230cf8c7c6948fda6b143e3309239ca
Naman <namansingla830@gmail.com>
2026-01-26 17:48:30 +0530

    navbar completed

--- frontend/src/pages/landing/sections/Navbar.jsx
+++ frontend/src/pages/landing/sections/Navbar.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
-import Logo from "../../../assets/logo/Youtube_logo.png"
+import Logo from "../../../assets/logo/Youtube_logo.png";
 
 const Navbar = () => {
   return (
-    <nav className="navbar_container w-full py-5 fixed bg-black text-white" >
-        <div className="navbar flex justify-between items-center mx-16">
-            <div className="navbar_logo flex items-center gap-2">
-                <figure className='w-12 '>
-                    <img src={Logo} alt="navbar logo" className='img'/>
-                </figure>
-                <h2 className='text-[22px] leading-6 font-bold'>CREATORSTUDIO</h2>
-            </div>
-
-            <div className="navbar_cta flex items-center gap-8">
-                <Link to="/" className='text-[16px] font-medium leading-5'>Log In</Link>
-                <Link to="/" className='btn_primary'>Sign Up Free</Link>
-            </div>
+    <nav className="navbar_container w-full py-5 fixed bg-black text-white">
+      <div className="navbar flex justify-between items-center lg:mx-16 sm:mx-6 mx-2">
+        <div className="navbar_logo flex items-center gap-1 justify-center">
+          <figure className="sm:w-12 w-8 ">
+            <img src={Logo} alt="navbar logo" className="img" />
+          </figure>
+          <h2 className="sm:text-[22px] text-[16px] leading-6 font-bold ">
+            CREATORSTUDIO
+          </h2>
         </div>
-    </nav>
-  )
-}
 
-export default Navbar
+        <div className="navbar_cta flex items-center sm:gap-8 gap-4 ">
+          <Link
+            to="/"
+            className="navbar_logIn sm:text-[16px] text-[12px] font-medium leading-5"
+          >
+            Log In
+          </Link>
+          <Link to="/" className="btn_primary">
+            Sign Up Free
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
