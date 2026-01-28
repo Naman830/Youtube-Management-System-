cc532940900f05f7a3b043dac6c42f381c7ff68c
Naman <namansingla830@gmail.com>
2026-01-28 14:46:44 +0530

    slider completyed

--- frontend/src/pages/landing/sections/Slider.jsx
+++ frontend/src/pages/landing/sections/Slider.jsx
@@ -1,4 +1,5 @@
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa6";
+import SliderImg from "../../../assets/images/slider.png";
 import React, { useRef, useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -8,67 +9,123 @@ import "swiper/css/navigation";
 import { Autoplay, Navigation } from "swiper/modules";
 
 export default function App() {
-    const prevRef = useRef(null);
-    const nextRef = useRef(null);
-    const [swiperInstance, setSwiperInstance] = useState(null);
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
+  const [swiperInstance, setSwiperInstance] = useState(null);
 
-    useEffect(() => {
-        if (
-            swiperInstance &&
-            prevRef.current &&
-            nextRef.current
-        ) {
-            swiperInstance.params.navigation.prevEl = prevRef.current;
-            swiperInstance.params.navigation.nextEl = nextRef.current;
+  useEffect(() => {
+    if (swiperInstance && prevRef.current && nextRef.current) {
+      swiperInstance.params.navigation.prevEl = prevRef.current;
+      swiperInstance.params.navigation.nextEl = nextRef.current;
 
-            swiperInstance.navigation.destroy();
-            swiperInstance.navigation.init();
-            swiperInstance.navigation.update();
-        }
-    }, [swiperInstance]);
+      swiperInstance.navigation.destroy();
+      swiperInstance.navigation.init();
+      swiperInstance.navigation.update();
+    }
+  }, [swiperInstance]);
 
-    return (
-        <div className="slider-wrapper">
-            {/* Header + arrows */}
-            <div className="slider-arrowsr flex justify-between items-center mb-5 max-w-7xl mx-auto px-5">
-                <h2 className="text-3xl font-medium">
-                    From Idea to Upload
-                </h2>
+  return (
+    <div className="slider-wrapper ">
+      {/* Header + arrows */}
+      <div className="slider-arrowsr flex justify-between items-center mb-5 max-w-7xl mx-auto md:px-5 sm:px-3">
+        <h2 className="md:text-3xl sm:text-2xl text-[20px] sm:leading-0 leading-6 font-medium">
+          From Idea to Upload
+        </h2>
 
-                <div className="flex gap-3">
-                    <button ref={prevRef} className="arrow-btn text-[20px] mr-2 text-[var(--gray-color)] hover:text-white transition-all duration-300">
-                        <FaArrowLeft />
-                    </button>
-                    <button ref={nextRef} className="arrow-btn text-[20px] mr-2 text-white">
-                        <FaArrowRight />
-                    </button>
-                </div>
-            </div>
-
-            <Swiper
-                onSwiper={setSwiperInstance}
-                slidesPerView={1}
-                spaceBetween={30}
-                loop
-                speed={700}
-                autoplay={{
-                    delay: 1400,
-                    disableOnInteraction: false,
-                    pauseOnMouseEnter: true,
-                }}
-                modules={[Autoplay, Navigation]}
-                className="mySwiper"
-            >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
-            </Swiper>
+        <div className="flex gap-3">
+          <button
+            ref={prevRef}
+            className="arrow-btn text-[20px] mr-2 text-[var(--gray-color)] hover:text-white transition-all duration-300"
+          >
+            <FaArrowLeft />
+          </button>
+          <button
+            ref={nextRef}
+            className="arrow-btn text-[20px] mr-2 text-white"
+          >
+            <FaArrowRight />
+          </button>
         </div>
-    );
+      </div>
+
+      <Swiper
+        onSwiper={setSwiperInstance}
+        slidesPerView={1}
+        spaceBetween={12}
+        loop
+        speed={700}
+        autoplay={{
+          delay: 900,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            centeredSlides: false,
+          },
+          1080: {
+            slidesPerView: 3,
+            centeredSlides: true,
+          },
+         
+        }}
+        modules={[Autoplay, Navigation]}
+      >
+        <SwiperSlide>
+          <figure className="slider_image">
+            <img src={SliderImg} alt="" className="img" />
+          </figure>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <figure className="slider_image">
+            <img src={SliderImg} alt="" className="img" />
+          </figure>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <figure className="slider_image">
+            <img src={SliderImg} alt="" className="img" />
+          </figure>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <figure className="slider_image">
+            <img src={SliderImg} alt="" className="img" />
+          </figure>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <figure className="slider_image">
+            <img src={SliderImg} alt="" className="img" />
+          </figure>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <figure className="slider_image">
+            <img src={SliderImg} alt="" className="img" />
+          </figure>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <figure className="slider_image">
+            <img src={SliderImg} alt="" className="img" />
+          </figure>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <figure className="slider_image">
+            <img src={SliderImg} alt="" className="img" />
+          </figure>
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <figure className="slider_image">
+            <img src={SliderImg} alt="" className="img" />
+          </figure>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
