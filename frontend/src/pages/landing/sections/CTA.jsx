3df5308441e26c2a3f84ce6552ccf6c48ebf17d1
Naman <namansingla830@gmail.com>
2026-02-02 15:42:36 +0530

    button and text of cta done

--- frontend/src/pages/landing/sections/CTA.jsx
+++ frontend/src/pages/landing/sections/CTA.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import {Link} from 'react-router-dom';
 
 const CTA = () => {
   return (
     <section className="cta_section relative h-full pb-24 pt-24 overflow-hidden text-white">
-      <div className="cta_container text-center px-5 ">
+      <div className="cta_container text-center px-5 mx-auto max-w-7xl">
         <div className="cta_text_container text-center mb-10">
           <p className="md:text-[22px] sm:text-[20px] md:leading-9 sm:leading-7 text-[18px] leading-5 font-regular mx-auto text-[var(--gray-color)] ">
             Final CTA Section
@@ -14,6 +14,10 @@ const CTA = () => {
           <p className="md:text-2xl sm:text-[22px] sm:leading-8 text-[20px] leading-6 font-regular mx-auto text-[var(--gray-color)] ">
             Join top creators building sustainable media businesses.
           </p>
+        </div>
+
+        <div>
+            <Link to="/" className="btn_primary cta_landing_button">JOIN THE PLATFORM</Link>
         </div>
        
       </div>
