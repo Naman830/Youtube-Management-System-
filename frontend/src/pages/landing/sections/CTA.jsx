21da1956becd5ca02fa623da93ddd434915702e1
Naman <namansingla830@gmail.com>
2026-02-02 16:30:17 +0530

    cta secction completed

--- frontend/src/pages/landing/sections/CTA.jsx
+++ frontend/src/pages/landing/sections/CTA.jsx
@@ -1,8 +1,8 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const CTA = () => {
   return (
-    <section className="cta_section relative h-full pb-24 pt-24 overflow-hidden text-white">
+    <section className="cta_section relative h-full pb-5 pt-24 overflow-hidden text-white">
       <div className="cta_container text-center px-5 mx-auto max-w-7xl">
         <div className="cta_text_container text-center mb-10">
           <p className="md:text-[22px] sm:text-[20px] md:leading-9 sm:leading-7 text-[18px] leading-5 font-regular mx-auto text-[var(--gray-color)] ">
@@ -16,13 +16,22 @@ const CTA = () => {
           </p>
         </div>
 
-        <div>
-            <Link to="/" className="btn_primary cta_landing_button">JOIN THE PLATFORM</Link>
+        <div className="mb-10">
+          <Link to="/" className="btn_primary cta_landing_button">
+            JOIN THE PLATFORM
+          </Link>
         </div>
-       
+
+        <div className="CTA_links_text flex flex-wrap justify-center gap-6 mt-10 text-[var(--gray-color)] text[16px] leading-5 font-regular ">
+          <Link to="/">Privacy Policy</Link>
+          <Link to="/">Support</Link>
+          <Link to="/">Carrers</Link>
+          <Link to="/">Contact</Link>
+          <Link to="/">Terms</Link>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
